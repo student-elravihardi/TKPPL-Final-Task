8f70f7c065ed37f00b6df52d4f98cebb13d286e4
161113427 <161113427@students.mikroskil.ac.id>
2019-12-12 22:39:19 +0700

    Menambahkan theme dan memperbaiki section dan merapikan section Review, rating, feedback user dan instalasi produk

--- Presentasi/src/scripts/main.js
+++ Presentasi/src/scripts/main.js
@@ -6,13 +6,15 @@ var scale = require('bespoke-scale');
 var bullets = require('bespoke-bullets');
 var hash = require('bespoke-hash');
 var extern = require('bespoke-extern');
+var carousel = require('bespoke-theme-carousel');
 
 // Bespoke.js
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
-  classes(),
-  nav(),
-  scale(),
-  bullets('.build, .build-items > *:not(.build-items)'),
-  hash(),
-  extern(bespoke)
+    classes(),
+    carousel(),
+    nav(),
+    scale(),
+    bullets('.build, .build-items > *:not(.build-items)'),
+    hash(),
+    extern(bespoke)
 ]);
